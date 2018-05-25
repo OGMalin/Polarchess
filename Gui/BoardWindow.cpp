b4227d2af6ad0cb053a2991780c92c718d7e012d
Odd Gunnar Malin <odd-gunnar.malin@varanger-kraft.no>
2018-05-25 13:17:10 +0200

    Init

--- Gui/BoardWindow.cpp
+++ Gui/BoardWindow.cpp
@@ -1,48 +1,128 @@
 #include "BoardWindow.h"
+#include <QPaintEvent>
 #include <QPainter>
 #include "../Common/defs.h"
 
 BoardWindow::BoardWindow(QWidget* parent)
 	:QWidget(parent)
 {
-	setBoardStyle();
+	whiteAtBottom = true;
+	setBoardTheme();
 	setMinimumSize(200, 200);
 }
 
 void BoardWindow::paintEvent(QPaintEvent* event)
 {
-	int i, j, x, y;
-	int size = qMin(width(), height());
-	squareSize = (size-4)/9;
-	int boardSize = (squareSize * 9) + 2;
-	int origoX = (width() - boardSize) / 2;
-	int origoY = (height() - boardSize) / 2;
+	calculateBoard();
+
+	if (!event->rect().intersects(borderRect))
+		return;
 
 	QPainter painter(this);
 
-	painter.drawRect(origoX, origoY, boardSize, boardSize);
-	painter.fillRect(origoX+1, origoY+1, squareSize * 9+2, squareSize * 9+2, borderBrush);
-	painter.drawRect(origoX + (squareSize / 2) + 1, origoY + (squareSize / 2) + 1, squareSize * 8 + 2, squareSize * 8 + 2);
-	bool light = true;
+	drawBorder(event, painter);
+	drawBoard(event, painter);
+	drawCoordinates(event, painter);
+}
+
+void BoardWindow::setBoardTheme()
+{
+	theme.lightBrush.setColor(QColor(255,255,191,255));
+	theme.lightBrush.setStyle(Qt::SolidPattern);
+	theme.darkBrush.setColor(QColor(127, 127, 63, 255));
+	theme.darkBrush.setStyle(Qt::SolidPattern);
+	theme.borderBrush.setColor(QColor(200, 200, 63, 255));
+	theme.borderBrush.setStyle(Qt::SolidPattern);
+	theme.coordinateFont.setBold(true);
+	theme.coordinateFontColor = QColor("white");
+}
+
+void BoardWindow::drawBorder(QPaintEvent* event, QPainter& painter)
+{
+	painter.drawRect(borderRect);
+	painter.fillRect(borderRect.x() + 1, borderRect.y() + 1, borderRect.width()-2, borderRect.height()-2, theme.borderBrush);
+	painter.drawRect(boardRect.x() - 1, boardRect.y() -1, boardRect.width()+2, boardRect.height()+2);
+}
+
+void BoardWindow::drawCoordinates(QPaintEvent* event, QPainter& painter)
+{
+	QRect sqF,sqR;
+	QString f,r;
+	int i;
+	theme.coordinateFont.setPixelSize(squareSize / 4);
+	painter.setFont(theme.coordinateFont);
+	painter.setPen(theme.coordinateFontColor);
+	if (whiteAtBottom)
+	{
+		f = "abcdefgh";
+		r = "87654321";
+	}
+	else
+	{
+		f = "hgfedcba";
+		r = "12345678";
+	}
+	sqF.setX(boardRect.x());
+	sqF.setY(boardRect.y() + boardRect.height() + 1);
+	sqF.setHeight(squareSize / 2);
+	sqF.setWidth(squareSize);
+	sqR.setX(borderRect.x() + 1);
+	sqR.setY(boardRect.y());
+	sqR.setHeight(squareSize);
+	sqR.setWidth(squareSize / 2);
 	for (i = 0; i < 8; i++)
 	{
-		y = origoY+(squareSize / 2)+2 + squareSize * i;
-		for (j = 0; j < 8; j++)
-		{
-			x = origoX + (squareSize / 2) + 2 + squareSize * j;
-			painter.fillRect(x, y, squareSize, squareSize,light?lightBrush:darkBrush);
-			light = light ? false : true;;
-		}
-		light = light ? false : true;;
+		painter.drawText(sqF, f.at(i),QTextOption(Qt::AlignCenter));
+		painter.drawText(sqR, r.at(i), QTextOption(Qt::AlignCenter));
+		sqF.translate(squareSize, 0);
+		sqR.translate(0, squareSize);
 	}
 }
 
-void BoardWindow::setBoardStyle()
+void BoardWindow::drawBoard(QPaintEvent* event, QPainter& painter)
 {
-	lightBrush.setColor(QColor(255,255,191,255));
-	lightBrush.setStyle(Qt::SolidPattern);
-	darkBrush.setColor(QColor(127, 127, 63, 255));
-	darkBrush.setStyle(Qt::SolidPattern);
-	borderBrush.setColor(QColor(63, 63,31, 255));
-	borderBrush.setStyle(Qt::SolidPattern);
+
+	int sq;
+	QRect sqR;
+	bool light = true;
+	for (sq=0;sq<64;sq++)
+	{
+		sqR = squareRect(sq);
+		if (event->rect().intersects(sqR))
+			painter.fillRect(sqR, (SQUARECOLOR(SQUARE128(sq))==WHITE) ? theme.lightBrush : theme.darkBrush);
+	}
+}
+
+void BoardWindow::calculateBoard()
+{
+	int size = qMin(width(), height());
+	squareSize = (((size - 4) / 9)/2)*2;
+	int w = squareSize * 9 + 4;
+
+	borderRect.setX((width() - w) / 2);
+	borderRect.setY((height() - w) / 2);
+
+	borderRect.setWidth(squareSize * 9 + 4);
+	borderRect.setHeight(borderRect.width());
+
+	boardRect.setX(borderRect.x() + squareSize / 2 + 2);
+	boardRect.setY(borderRect.y() + squareSize / 2 + 2);
+	boardRect.setWidth(squareSize * 8);
+	boardRect.setHeight(boardRect.width());
+}
+
+QRect BoardWindow::squareRect(int sq)
+{
+	QRect rect;
+	int file = FILE(SQUARE128(sq));
+	int rank = RANK(SQUARE128(sq));
+	if (whiteAtBottom)
+		rank = abs(rank - 7);
+	else
+		file = abs(file - 7);
+	rect.setX(boardRect.x() + file * squareSize);
+	rect.setY(boardRect.y() + rank * squareSize);
+	rect.setWidth(squareSize);
+	rect.setHeight(squareSize);
+	return rect;
 }
--- Gui/BoardWindow.h
+++ Gui/BoardWindow.h
@@ -4,6 +4,15 @@
 //#include <QTextEdit>
 #include "../Common/ChessBoard.h"
 
+struct BOARDTHEMA
+{
+	QBrush lightBrush;
+	QBrush darkBrush;
+	QBrush borderBrush;
+	QFont  coordinateFont;
+	QColor coordinateFontColor;
+};
+
 class BoardWindow :public QWidget
 {
 	Q_OBJECT
@@ -13,10 +22,16 @@ protected:
 	void paintEvent(QPaintEvent* event) override;
 private:
 	ChessBoard currentBoard;
+	bool whiteAtBottom;
 	int squareSize;
-	QBrush lightBrush;
-	QBrush darkBrush;
-	QBrush borderBrush;
+	QRect boardRect;
+	QRect borderRect;
+	BOARDTHEMA theme;
 
-	void setBoardStyle();
+	void drawBorder(QPaintEvent* event, QPainter& painter);
+	void drawCoordinates(QPaintEvent* event, QPainter& painter);
+	void drawBoard(QPaintEvent* event, QPainter& painter);
+	void setBoardTheme();
+	void calculateBoard();
+	QRect squareRect(int sq);
 };
