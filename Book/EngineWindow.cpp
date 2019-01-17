47ffea2aacb79f2fdec15e54c6771f3ddeaeccf0
Odd Gunnar Malin <odd-gunnar.malin@varanger-kraft.no>
2019-01-17 15:08:39 +0100

    *

--- Book/EngineWindow.cpp
+++ Book/EngineWindow.cpp
@@ -1,11 +1,21 @@
 #include "EngineWindow.h"
-#include <QBoxLayout>
+#include <QGridLayout>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
 
 EngineWindow::EngineWindow(QWidget *parent)
 	: QWidget(parent)
 {
-	QBoxLayout* box = new QBoxLayout(QBoxLayout::LeftToRight);
-	setLayout(box);
+	QGridLayout* grid = new QGridLayout;
+	QTextEdit* output = new QTextEdit;
+	QLineEdit* lines = new QLineEdit;
+	QPushButton* analyze = new QPushButton("Analyze");
+	grid->addWidget(lines, 0, 0, Qt::AlignLeft);
+	grid->addWidget(analyze, 0, 1, Qt::AlignLeft);
+	grid->addWidget(output, 1, 0, -1, -1, Qt::AlignLeft);
+
+	setLayout(grid);
 }
 
 EngineWindow::~EngineWindow()
