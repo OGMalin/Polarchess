b94ef0a465d391c1b03e939bf502d36c58f22fcb
Odd Gunnar Malin <odd-gunnar.malin@varanger-kraft.no>
2020-03-06 11:45:18 +0100

    *

--- Installer/Create PolarBook.cmd
+++
@@ -1,8 +0,0 @@
-del /s /q C:\Prosjekt\cpp\PolarChess\Installer\packages\com.polarchess.polarbook\data\*.*
-copy C:\Prosjekt\cpp\Polarchess\x64\Release\Book.exe C:\Prosjekt\cpp\PolarChess\Installer\packages\com.polarchess.polarbook\data
-call C:\qt\5.14.1\msvc2017_64\bin\qtenv2.bat
-cd \prosjekt\cpp\PolarChess\Installer
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-windeployqt --compiler-runtime --force C:\Prosjekt\cpp\PolarChess\Installer\packages\com.polarchess.polarbook\data\Book.exe
-C:\Qt\Tools\QtInstallerFramework\3.2\bin\binarycreator.exe --offline-only -c config\config.xml -p packages PolarChessInstaller
-pause
